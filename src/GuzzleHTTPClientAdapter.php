789f6e13ca2ebe19300604e310fd972c62f97002
Janos Pasztor <janos@pasztor.at>
2016-09-08 15:41:18 +0200

    Initial import

---
+++ src/GuzzleHTTPClientAdapter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Ixolit\Dislo\HTTP\Guzzle;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Uri;
+use Ixolit\Dislo\HTTP\HTTPClientAdapter;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+
+class GuzzleHTTPClientAdapter implements HTTPClientAdapter {
+	/**
+	 * @return RequestInterface
+	 */
+	public function createRequest() {
+		return new Request('GET', '/');
+	}
+
+	/**
+	 * @return UriInterface
+	 */
+	public function createUri() {
+		return new Uri();
+	}
+
+	/**
+	 * @param string $string
+	 *
+	 * @return StreamInterface
+	 */
+	public function createStringStream($string) {
+		return \GuzzleHttp\Psr7\stream_for($string);
+	}
+
+	/**
+	 * @param RequestInterface $request
+	 *
+	 * @return ResponseInterface
+	 */
+	public function send(RequestInterface $request) {
+		$client = new Client();
+		try {
+			return $client->send($request);
+		} catch (ClientException $e) {
+			return $e->getResponse();
+		}
+	}
+}
