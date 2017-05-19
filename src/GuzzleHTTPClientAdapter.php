2117190dbfb602df8d759b7ab4674e77ff30a0bc
Markus Marchewa <m.marchewa@ixolit.com>
2017-05-19 16:54:01 +0200

    added HTTPClientAdapterEx

--- src/GuzzleHTTPClientAdapter.php
+++ src/GuzzleHTTPClientAdapter.php
@@ -7,13 +7,16 @@ use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Uri;
+use GuzzleHttp\RequestOptions;
 use Ixolit\Dislo\HTTP\HTTPClientAdapter;
+use Ixolit\Dislo\HTTP\HTTPClientAdapterEx;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\UriInterface;
 
-class GuzzleHTTPClientAdapter implements HTTPClientAdapter {
+class GuzzleHTTPClientAdapter implements HTTPClientAdapter, HTTPClientAdapterEx {
+
 	/**
 	 * @return RequestInterface
 	 */
@@ -46,6 +49,32 @@ class GuzzleHTTPClientAdapter implements HTTPClientAdapter {
 		$client = new Client();
 		try {
 			return $client->send($request);
+		} catch (ServerException $e) {
+			return $e->getResponse();
+		} catch (ClientException $e) {
+			return $e->getResponse();
+		}
+	}
+
+	/**
+	 * @param RequestInterface $request
+	 * @param array $options
+	 *
+	 * @return ResponseInterface
+	 */
+	public function sendAdvanced(RequestInterface $request, array $options) {
+		$client = new Client();
+		try {
+			// map interface options to client specific ones
+			$clientOptions = [];
+			foreach ($options as $key => $value) {
+				switch ($key) {
+					case HTTPClientAdapterEx::OPTION_RESPONSE_BODY_STREAM:
+						$clientOptions[RequestOptions::SINK] = \GuzzleHttp\Psr7\stream_for($value);
+						break;
+				}
+			}
+			return $client->send($request, $clientOptions);
 		} catch (ServerException $e) {
 			return $e->getResponse();
 		} catch (ClientException $e) {
